6722d8461fe58f1759f6e8e203d325588432113e
SitaSeth <sita@sethurajan.co.uk>
2022-11-17 07:27:30 +0000

    Fix view error

--- things/urls.py
+++ things/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path
 from things import views
 urlpatterns = [
-   path('',views.home,name='home'),
+   path('/',views.home,name='home'),
 ]
